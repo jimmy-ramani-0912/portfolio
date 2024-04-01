b5a23e2effb01c39ff3f97693714b9120e59eb29
Jimmy Ramani <jimmy.ramani0912@gmail.com>
2024-04-01 16:47:29 +0530

    Changes

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,51 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import svgrPlugin from "vite-plugin-svgr";
+// import envCompatible from "vite-plugin-env-compatible";
+// const path = require("path");
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   envPrefix: "REACT_APP_",
+//   // This changes the out put dir from dist to build
+//   // comment this out if that isn't relevant for your project
+//   build: {
+//     outDir: "build",
+//   },
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+//   plugins: [
+//     react(),
+//     envCompatible(),
+//     svgrPlugin({
+//       svgrOptions: {
+//         icon: true,
+//         // ...svgr options (https://react-svgr.com/docs/options/)
+//       },
+//     }),
+//   ],
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import svgrPlugin from "vite-plugin-svgr";
-import envCompatible from "vite-plugin-env-compatible";
-const path = require("path");
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  envPrefix: "REACT_APP_",
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
-  build: {
-    outDir: "build",
-  },
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      // "@assets": "/src/assets",
+      // "@components": "/src/components",
+      // "@pages": "/src/pages",
     },
   },
-  plugins: [
-    react(),
-    envCompatible(),
-    svgrPlugin({
-      svgrOptions: {
-        icon: true,
-        // ...svgr options (https://react-svgr.com/docs/options/)
-      },
-    }),
-  ],
+  server: {
+    host: true,
+    port: 3001,
+    watch: {
+      usePolling: false,
+    },
+  },
 });
