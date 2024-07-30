d7559753bb30ba8cf8f15516ab39e0c61528baf2
Jimmy Ramani <jimmy.ramani0912@gmail.com>
2024-07-30 18:02:53 +0530

    Improving navbar navigating

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,8 @@ import react from "@vitejs/plugin-react";
 import svgrPlugin from "vite-plugin-svgr";
 import envCompatible from "vite-plugin-env-compatible";
 import path from "path";
-// const path = require("path");
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/portfolio/",
   envPrefix: "REACT_APP_",
   esbuild: {
@@ -16,18 +13,6 @@ export default defineConfig({
   // build: {
   //   outDir: "build",
   // },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return "vendor";
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000,
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
